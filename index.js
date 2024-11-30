70a41edc9a128578e20e710682cab6c302a97942
ishitashete06 <113773448+ishitashete06@users.noreply.github.com>
2024-11-30 23:45:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://hack2hire-bytecraft-2.onrender.com',
     credentials:true
 }
 
@@ -41,7 +41,7 @@ app.use('/api/tasks', taskRoutes);
 
 
 app.use(cors({
-    origin: "http://localhost:5173", // replace with your frontend URL
+    origin: "https://hack2hire-bytecraft-2.onrender.com", // replace with your frontend URL
     credentials: true,  // allow cookies if needed
   }));
   
